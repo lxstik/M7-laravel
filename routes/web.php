c47eb137180f0bf5b9267be60ac400447e345fe2
Yehor Fal <egorrfal32@gmail.com>
2025-04-02 16:55:04 +0000

    componentes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,25 @@
 <?php
-
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('peliculas');
+});
+
+
+Route::get('/suma', function () {
+    return view('suma');
+});
+
+Route::post('/suma', function (Request $request) {
+    $num1 = $request->input('num1');
+    $num2 = $request->input('num2');
+    $resultado = $num1 + $num2;
+
+    return view('suma', ['resultado' => $resultado]);
 });
