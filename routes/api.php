8a8de9ed5b16445946578c7b8b44de607cf34877
Yehor Fal <egorrfal32@gmail.com>
2025-04-09 15:34:49 +0000

    Routes

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/students', function () { return 'Student list'; });
+
+Route::post('/students', function () { return 'Creating student'; });
+
+Route::put('/students/{id}', function () { return 'Updating student'; });
+
+Route::delete('/students/{id}', function () { return 'Deleti student'; });
+
+Route::get('/students/{id}', function () {return 'Getting one student'; });
